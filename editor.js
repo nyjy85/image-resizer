c6cc9b6605aa5c701392814feb7451a600066bc7
Joanne Yae <yae.joanne@gmail.com>
2015-07-24 21:37:00 -0400

    she worksgit add .git add .

--- editor.js
+++ editor.js
@@ -3,49 +3,37 @@ var imagemin = require('gulp-imagemin');
 var imageResize = require('gulp-image-resize');
 var rename = require("gulp-rename");
 var lwip = require('gulp-lwip');
+var addsrc = require('gulp-add-src');
 
 function processImg (filesrc) {
   console.log('FILESRC BT', filesrc)
- return gulp.src(filesrc)
 
-  // compress and save
-  .pipe(imagemin({optimizationLevel: 5}))
- 
-  .pipe(lwip
-    .scale(0.5)
-    .resize(300, 250)
-  )
+ return gulp.src(filesrc)
+  .pipe(lwip.resize(300, 250))
   .pipe(rename(filesrc.slice(22).replace(/\./, "300.")))
   .pipe(gulp.dest('public/images/'+filesrc.slice(22, 54)))
-  // save 120 x 120
-  .pipe(lwip
-    .scale(0.5)
-    .resize(336, 280)
-  )
-  .pipe(rename(filesrc.slice(22).replace(/\./, "336.")))
-  .pipe(gulp.dest('public/images/'+filesrc.slice(22, 54)))
 
-  .pipe(lwip
-    .scale(0.5)
-    .resize(728, 90)
-  )
-  .pipe(rename(filesrc.slice(22).replace(/\./, "728.")))
-  .pipe(gulp.dest('public/images/'+filesrc.slice(22, 54)))
+  .pipe(addsrc(filesrc)
+    .pipe(lwip.resize(336, 280))
+    .pipe(rename(filesrc.slice(22).replace(/\./, "336.")))
+    .pipe(gulp.dest('public/images/'+filesrc.slice(22, 54))))
   
-  .pipe(lwip
-    .scale(0.5)
-    .resize(300, 600)
-  )
-  .pipe(rename(filesrc.slice(22).replace(/\./, "600.")))
-  .pipe(gulp.dest('public/images/'+filesrc.slice(22, 54)))
+  .pipe(addsrc(filesrc)
+    .pipe(lwip.resize(728, 90))
+    .pipe(rename(filesrc.slice(22).replace(/\./, "728.")))
+    .pipe(gulp.dest('public/images/'+filesrc.slice(22, 54))))
   
-  .pipe(lwip
-    .scale(0.5)
-    .resize(320, 180)
-  )
-  .pipe(rename(filesrc.slice(22).replace(/\./, "320.")))
-  .pipe(gulp.dest('public/images/'+filesrc.slice(22, 54)))
+  .pipe(addsrc(filesrc)
+    .pipe(lwip.resize(300, 600))
+    .pipe(rename(filesrc.slice(22).replace(/\./, "600.")))
+    .pipe(gulp.dest('public/images/'+filesrc.slice(22, 54))))
+  
+  .pipe(addsrc(filesrc)
+    .pipe(lwip.resize(320, 180))
+    .pipe(rename(filesrc.slice(22).replace(/\./, "320.")))
+    .pipe(gulp.dest('public/images/'+filesrc.slice(22, 54))))
 }
+
 
 process.on('message', function (images) {
   console.log('Image processing started...');
